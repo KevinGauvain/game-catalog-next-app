b6a3b30ce9b7a6820025bf3c93f89c1bbf2ad324
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-03-23 15:44:53 +0100

    bug git re-push

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,47 +2,47 @@
 // import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
-// import Layout from "./layout";
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Layout>
-//         <p>INDEX</p>
-//       </Layout>
-//     </div>
-//   );
-// }
-
 import Layout from "./layout";
-import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Home() {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  if (user) {
-    return (
-      <div>
-        <div>
-          Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-        </div>
-        <Layout>
-          <p>INDEX TEST</p>
-        </Layout>
-      </div>
-    );
-  }
   return (
     <div>
-      <div>
-        <a href="/api/auth/login">Login</a>
-      </div>
       <Layout>
-        <p>INDEX TEST</p>
+        <p>INDEX</p>
       </Layout>
     </div>
   );
 }
+
+// import Layout from "./layout";
+// import { useUser } from "@auth0/nextjs-auth0";
+
+// export default function Home() {
+//   const { user, error, isLoading } = useUser();
+
+//   if (isLoading) return <div>Loading...</div>;
+//   if (error) return <div>{error.message}</div>;
+
+//   if (user) {
+//     return (
+//       <div>
+//         <div>
+//           Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+//         </div>
+//         <Layout>
+//           <p>INDEX TEST</p>
+//         </Layout>
+//       </div>
+//     );
+//   }
+//   return (
+//     <div>
+//       <div>
+//         <a href="/api/auth/login">Login</a>
+//       </div>
+//       <Layout>
+//         <p>INDEX TEST</p>
+//       </Layout>
+//     </div>
+//   );
+// }
