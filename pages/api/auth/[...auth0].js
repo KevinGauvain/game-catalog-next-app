e38db0d76b32cb59d906ae2f3e4b298226847972
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-03-23 14:56:35 +0100

    auth0 logIn et logOut fonctionnel (en partie)

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,12 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
 export default handleAuth();
+
+// GET https://YOUR_DOMAIN/v2/logout?
+//   client_id=YOUR_CLIENT_ID&
+//   returnTo=LOGOUT_URL
+
+// app.get("https://game-catalog-website-k/logout?") => {
+//   client_id=process.env.AUTH0_CLIENT_ID;
+//   returnTo="http://localhost:3000/games";
+// }
