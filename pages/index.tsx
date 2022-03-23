03f57f7ec5e0c9bf53f5389c8c0b4103d0a80ce5
Kevin Gauvain <kevin.gauvain@decathlon.com>
2022-03-23 16:13:18 +0100

    Auth0 amélioré avec bouton et welcoming

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,36 +13,3 @@ export default function Home() {
     </div>
   );
 }
-
-// import Layout from "./layout";
-// import { useUser } from "@auth0/nextjs-auth0";
-
-// export default function Home() {
-//   const { user, error, isLoading } = useUser();
-
-//   if (isLoading) return <div>Loading...</div>;
-//   if (error) return <div>{error.message}</div>;
-
-//   if (user) {
-//     return (
-//       <div>
-//         <div>
-//           Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-//         </div>
-//         <Layout>
-//           <p>INDEX TEST</p>
-//         </Layout>
-//       </div>
-//     );
-//   }
-//   return (
-//     <div>
-//       <div>
-//         <a href="/api/auth/login">Login</a>
-//       </div>
-//       <Layout>
-//         <p>INDEX TEST</p>
-//       </Layout>
-//     </div>
-//   );
-// }
